106e3b4bc36244497939a68287fcae00e81c9184
vinicius.poli <alemaumvp@hotmail.com>
2026-08-17 14:52:36 -0300

    Adicionado Aplicacao Garcom

--- src/ControleDeBar.Aplicacao/InjecaoDependencia.cs
+++ src/ControleDeBar.Aplicacao/InjecaoDependencia.cs
@@ -1,3 +1,4 @@
+using ControleDeBar.Aplicacao.Modulos.ModuloGarcom;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +11,6 @@ public static class InjecaoDependencia
         IConfiguration configuration
     )
     {          
-        
+        services.AddScoped<ServicoGarcom>();
     }
 }
