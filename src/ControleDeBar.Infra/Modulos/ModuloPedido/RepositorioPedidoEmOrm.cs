9e9e4d188c1acf42680d8e7fd0ca0317654efdcc
Alexandre Rocha <juny14_15@hotmail.com>
2026-08-23 11:34:27 -0300

    Adiciona Modulo Pedido em Infra

---
+++ src/ControleDeBar.Infra/Modulos/ModuloPedido/RepositorioPedidoEmOrm.cs
@@ -0,0 +1,10 @@
+using ControleDeBar.Dominio.Modulos.ModuloPedido;
+using ControleDeBar.Infra.Compartilhado.Orm;
+
+namespace ControleDeBar.Infra.Modulos.ModuloPedido;
+
+public sealed class RepositorioPedidoEmOrm(ControleDeBarDbContext dbContext) :
+    RepositorioBaseEmOrm<Pedido>(dbContext), IRepositorioPedido
+{
+
+}
