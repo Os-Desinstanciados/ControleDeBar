61b5128c11e45604c2a2804c418b303feca95144
Alexandre Rocha <juny14_15@hotmail.com>
2026-08-23 15:48:39 -0300

    Corrige migrations após integração da autenticação

--- src/ControleDeBar.Infra/Migrations/20260823182939_AtualizarModeloAposMerge.cs
+++ src/ControleDeBar.Infra/Migrations/20260823182939_AtualizarModeloAposMerge.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace ControleDeBar.Infra.Migrations
 {
     /// <inheritdoc />
-    public partial class AtualizacaoModel : Migration
+    public partial class AtualizarModeloAposMerge : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
